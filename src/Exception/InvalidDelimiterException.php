3cc58a87ac22ea3ae828d384cbfc3e4811db34e7
Ryan Howe <rhowe@kelleyryan.com>
2023-06-16 09:36:21 -0400

    3.x Initial dev commit

--- src/Exception/InvalidDelimiterException.php
+++ src/Exception/InvalidDelimiterException.php
@@ -6,6 +6,13 @@ use Throwable;
 
 class InvalidDelimiterException extends DotException {
 
+    /**
+     * Delimiters can be any string that is compatible with the explode() function.
+     *
+     * @param string         $delimiter
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
     public function __construct(string $delimiter = "", int $code = 0, ?Throwable $previous = null) {
         parent::__construct("Invalid delimiter, ryanwhowe/Dot is unable to function with the delimiter of '{$delimiter}'", $code, $previous);
     }
